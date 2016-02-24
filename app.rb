028302ff774c4415346ec64342f7b6be5e4e3a36
Julio César <jcesar@jovenclub.cu>
2016-02-23 16:12:48 -0800

    encio de formulario OK

--- app.rb
+++ app.rb
@@ -10,6 +10,32 @@ end
 
 
 
+#Configuracion de email
+post '/:locale/archievingsubcription' do
+  require 'pony'
+
+  from = "subcribe@archieving.com"
+  subject = "Archieving suscription"
+
+  Pony.mail(
+      :from => from,
+      :to => 'lovera@irstrat.com',
+      :headers => { 'Content-Type' => 'text/html' },
+      :body => erb(:"en/generales/mail"),
+      :via => :smtp,
+      :via_options => {
+          :address              => 'smtp.mailgun.org',
+          :port                 => '587',
+          :enable_starttls_auto => true,
+          :user_name            => 'postmaster@irstrat.com',
+          :password             => '5ptmod-dfz40',
+          :authentication       => :plain,
+          :domain               => "irstrat.com"
+      })
+  redirect '/'
+end
+
+
 
 # Globales
 
